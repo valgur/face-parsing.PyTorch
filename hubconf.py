f90f062d2a052d95bad7f1cb68752c5ba05f5a3c
Martin Valgur <martin.valgur@gmail.com>
2020-05-14 09:52:09 +0300

    add hubconf.py, use torch.hub

---
+++ hubconf.py
@@ -0,0 +1,15 @@
+dependencies = ['torch']
+
+import torch.hub
+
+from model import BiSeNet as _BiSeNet
+
+_pretrained_url = 'https://github.com/valgur/face-parsing.PyTorch/releases/download/v1.0/bisenet-468e13ca.pt'
+
+
+def BiSeNet(pretrained=False, progress=True, map_location=None, n_classes=19):
+    net = _BiSeNet(n_classes)
+    if pretrained:
+        net.load_state_dict(torch.hub.load_state_dict_from_url(
+            _pretrained_url, map_location=map_location, progress=progress, check_hash=True))
+    return net
--- resnet.py
+++ resnet.py
@@ -2,9 +2,9 @@
 # -*- encoding: utf-8 -*-
 
 import torch
+import torch.hub
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.model_zoo as modelzoo
 
 # from modules.bn import InPlaceABNSync as BatchNorm2d
 
@@ -80,7 +80,7 @@ class Resnet18(nn.Module):
         return feat8, feat16, feat32
 
     def init_weight(self):
-        state_dict = modelzoo.load_url(resnet18_url)
+        state_dict = torch.hub.load_state_dict_from_url(resnet18_url)
         self_state_dict = self.state_dict()
         for k, v in state_dict.items():
             if 'fc' in k: continue
